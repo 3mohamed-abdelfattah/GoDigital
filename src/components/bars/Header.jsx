f37bc55ec41034ec34c5462396c46b38f1462e8a
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-02 19:30:05 +0200

    hover Profile button

--- src/components/bars/Header.jsx
+++ src/components/bars/Header.jsx
@@ -7,10 +7,10 @@ export const Header = () => {
         <header className="flex justify-around items-center my-9">
 
             {/* Logo */}
-            <img src={Logo} alt='Go Digital Logo' width={190} height={69} />
+            <img src={Logo} alt='Go Digital Logo' width={140} />
 
             {/* Navigation Links */}
-            <ul className="flex gap-16 items-center font-semibold text-lg">
+            <ul className="flex gap-16 items-center font-semibold">
                 {['Αρχική', 'Προϊόντα', 'Σχετικά με εμάς', 'Blog', 'Επικοινωνία'].map((nav, index) =>
                     <li key={index} className="hover:bg-secondaryColor hover:text-white hover:rounded-3xl px-4 py-2 cursor-pointer">
                         {nav == 'Προϊόντα' ? <span className="flex items-center gap-1">{nav} <Icons.ToggleArrowIcon />  </span> : nav}
@@ -19,7 +19,7 @@ export const Header = () => {
             </ul>
 
             {/* User Profile Icon */}
-            <span className="flex justify-center items-center bg-secondaryColor rounded-full w-10 h-10">
+            <span className="flex justify-center items-center bg-secondaryColor hover:bg-primaryBgColor rounded-full w-10 h-10 cursor-pointer">
                 <Icons.UserIcon />
             </span>
 

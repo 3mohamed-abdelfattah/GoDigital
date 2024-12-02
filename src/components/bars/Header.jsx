8bf9af297166e7708463c9690c2c059b1c6893e6
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-02 19:27:11 +0200

    Header UI Done

--- src/components/bars/Header.jsx
+++ src/components/bars/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import Logo from '../../assets/images/logo.png'
+import * as Icons from '@/utils/icons.util'
 
 export const Header = () => {
     return (
-        <header className="">
-            Header
+        <header className="flex justify-around items-center my-9">
+
+            {/* Logo */}
+            <img src={Logo} alt='Go Digital Logo' width={190} height={69} />
+
+            {/* Navigation Links */}
+            <ul className="flex gap-16 items-center font-semibold text-lg">
+                {['Αρχική', 'Προϊόντα', 'Σχετικά με εμάς', 'Blog', 'Επικοινωνία'].map((nav, index) =>
+                    <li key={index} className="hover:bg-secondaryColor hover:text-white hover:rounded-3xl px-4 py-2 cursor-pointer">
+                        {nav == 'Προϊόντα' ? <span className="flex items-center gap-1">{nav} <Icons.ToggleArrowIcon />  </span> : nav}
+                    </li>
+                )}
+            </ul>
+
+            {/* User Profile Icon */}
+            <span className="flex justify-center items-center bg-secondaryColor rounded-full w-10 h-10">
+                <Icons.UserIcon />
+            </span>
+
         </header>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { Header } from '../components/bars/Header'
 
 export const HomePage = () => {
     return (
         <main>
+            <Header />
             Go Digital
         </main>
     )

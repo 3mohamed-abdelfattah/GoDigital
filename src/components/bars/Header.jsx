dccc24f2f0458554ab67afc3e418867f6a468e10
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-02 19:55:15 +0200

    Hero Card

--- src/components/bars/Header.jsx
+++ src/components/bars/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import Logo from '../../assets/images/logo.png'
+// Icons
 import * as Icons from '@/utils/icons.util'
+// Image
+import Logo from '@/assets/images/logo.png'
 
 export const Header = () => {
     return (
-        <header className="flex justify-around items-center my-9">
+        <header className="flex justify-around items-center my-9 mx-5">
 
             {/* Logo */}
             <img src={Logo} alt='Go Digital Logo' width={140} />
 
             {/* Navigation Links */}
-            <ul className="flex gap-16 items-center font-semibold">
+            <ul className="flex gap-14 items-center font-semibold">
                 {['Αρχική', 'Προϊόντα', 'Σχετικά με εμάς', 'Blog', 'Επικοινωνία'].map((nav, index) =>
                     <li key={index} className="hover:bg-secondaryColor hover:text-white hover:rounded-3xl px-4 py-2 cursor-pointer">
                         {nav == 'Προϊόντα' ? <span className="flex items-center gap-1">{nav} <Icons.ToggleArrowIcon />  </span> : nav}

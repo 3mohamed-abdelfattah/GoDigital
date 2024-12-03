c73d6524a652b950d5c5150cf944921fed022787
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-03 23:44:47 +0200

    Responsive Hero Section

--- src/components/custom/PlaneLabel.jsx
+++ src/components/custom/PlaneLabel.jsx
@@ -1,17 +1,17 @@
 // Icons
 import * as Icons from '@/utils/icons.util'
 
-export const PlaneLabel = ({ children }) => {
+export const PlaneLabel = ({ children, rotateBox, rotateText, hight = 'h-[165.31px]' }) => {
     return (
         <main
-            className="flex flex-col justify-evenly items-center w-[224.57px] h-[315.12px] rounded-[154.84px] bg-secondaryColor"
+            className={`${rotateBox} vsm:transform-none flex flex-col justify-evenly items-center w-[117.09px] md:w-[224.57px] ${hight} vsm:h-[184.31px] md:h-[315.12px] rounded-[154.84px] bg-secondaryColor`}
             style={{ boxShadow: 'inset 0 4px 4px rgba(0, 0, 0, 0.2)' }}
         >
-            <span className="flex justify-center items-center bg-secondaryBgColor rounded-full w-[147.63px] h-[147.63px]">
+            <span className={`${rotateText} vsm:transform-none flex justify-center items-center bg-secondaryBgColor rounded-full w-[76.98px] md:w-[147.63px] h-[76.98px] md:h-[147.63px]`}>
                 <Icons.PlaneIcon />
             </span>
             {/* Label Text*/}
-            <h3 className="max-w-[191px] font-bold text-lg leading-[24.59px] text-center py-2">{children}</h3>
+            <h3 className={`${rotateText} vsm:transform-none max-w-[191px] font-bold text-xs md:text-lg md:leading-[24.59px] text-center py-2`}>{children}</h3>
         </main>
     )
 }
--- src/components/forHome/HeroSection.jsx
+++ src/components/forHome/HeroSection.jsx
@@ -5,27 +5,27 @@ import Symbol from '@/assets/images/icon.png'
 
 export const HeroSection = () => {
     return (
-        <main className='bg-secondaryBgColor mx-20 rounded-[58px]'>
+        <main className='bg-secondaryBgColor mx-6 md:mx-20 rounded-3xl md:rounded-[58px]'>
 
             {/* First Hero Section [Logo & Titles] */}
             <section
-                className="flex flex-col justify-center items-center bg-primaryBgColor text-center rounded-[58px] text-primaryColor py-12 h-[590px]"
+                className="flex flex-col justify-center items-center bg-primaryBgColor text-center rounded-3xl md:rounded-[58px] text-primaryColor py-6 md:py-12 md:h-[590px]"
                 style={{ boxShadow: '0 10px 10px rgba(0, 0, 0, 0.2)' }}
             >
                 {/* Hero Section Logo */}
-                <img src={Symbol} alt='Go Digital Icon' width={199} />
+                <img src={Symbol} alt='Go Digital Icon' className="w-[100px] md:w-[199px]" />
                 {/* Hero Section Titles */}
-                <h1 className="max-w-[806px] text-6xl font-semibold leading-[81.96px]">Lorem ipsum dolor sit amet consectetur adipiscing</h1>
-                <h2 className="mt-5 mb-6 max-w-[519px] text-[22px] leading-[30.05px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
+                <h1 className="max-w-[806px] mx-5 text-2xl vsm:text-4xl md:text-6xl font-semibold vsm:leading-[51.96px] md:leading-[81.96px]">Lorem ipsum dolor sit amet consectetur adipiscing</h1>
+                <h2 className="mt-5 mb-6 mx-5 max-w-[519px] text-xs vsm:text-base md:text-[22px] vsm:leading-[30.05px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
             </section>
 
             {/* Second Hero Section [Plane Labels] */}
-            <section className="flex justify-center items-center gap-10 rounded-b-[58px] text-center text-primaryColor pt-[78px] pb-16 mx-20">
+            <section className="flex flex-wrap justify-center items-center gap-10 rounded-3xl md:rounded-[58px] text-center text-primaryColor pt-8 pb-8 md:pt-[78px] md:pb-16 mx-5 md:mx-20">
                 <PlaneLabel>Travel Insurance</PlaneLabel>
                 <PlaneLabel>Προσωπικών Ατυχημάτων Αλλοδαπών</PlaneLabel>
                 <PlaneLabel>Ευθύνης Ασφαλιστικών Διαμεσολαβητών</PlaneLabel>
                 <PlaneLabel>Ευθύνης Οδικού Μεταφορέα </PlaneLabel>
-                <PlaneLabel>Εγγυητικές Επιστολές</PlaneLabel>
+                <PlaneLabel rotateBox='rotate-90' hight='h-[241.8px]' rotateText='-rotate-90'>Εγγυητικές Επιστολές</PlaneLabel>
             </section>
 
         </main>

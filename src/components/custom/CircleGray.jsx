8954f70487678eed36cd244f83b2a6be488c9ebd
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-04 11:19:34 +0200

    to 300px width WhyDigital Section

--- src/components/custom/CircleGray.jsx
+++ src/components/custom/CircleGray.jsx
@@ -1,10 +1,10 @@
 export const CircleGray = ({ children }) => {
     return (
-        <span className="flex flex-col items-center gap-3 sm:gap-9">
+        <span className="flex flex-col items-center tiny:gap-3 sm:gap-9">
             {/* Circle */}
-            <span className="w-20 h-20 sm:w-[100px] sm:h-[100px] rounded-full bg-primaryBgColor"></span>
+            <span className="w-12 h-12 tiny:w-20 tiny:h-20 sm:w-[100px] sm:h-[100px] rounded-full bg-primaryBgColor"></span>
             {/* title */}
-            <h3 className="text-sm sm:text-xl sm:leading-[24.2px] text-center font-bold text-primaryColor max-w-[130px] sm:max-w-[221px]">{children}</h3>
+            <h3 className="text-xs tiny:text-xs sm:text-xl sm:leading-[24.2px] text-center font-bold text-primaryColor max-w-[130px] sm:max-w-[221px]">{children}</h3>
         </span>
     )
 }
--- src/components/forHome/WhyDigitalSection.jsx
+++ src/components/forHome/WhyDigitalSection.jsx
@@ -3,13 +3,13 @@ import { CircleGray } from '@/components'
 
 export const WhyDigitalSection = () => {
     return (
-        <main className="max-w-[1754px] xl:h-[615px] bg-secondaryColor rounded-3xl lg:rounded-[58px] text-center mt-10 md:mt-[139px] mx-7 lg:mx-20 p-8 lg:p-12">
+        <main className="max-w-[1754px] xl:h-[615px] bg-secondaryColor rounded-3xl lg:rounded-[58px] text-center mt-10 md:mt-[139px] mx-7 lg:mx-20 p-2 vsm:p-8 lg:p-12">
             {/* Head Titles */}
             <h1 className="text-primaryColor font-extrabold text-3xl sm:text-[40px]">Γιατί GoDigital;</h1>
             <h2 className="max-w-[911px] mt-3 text-primaryColor font-medium text-xs tiny:text-lg sm:text-[22px] sm:leading-[30.05px] mx-auto">Στην Go Digital απλοποιούμε την ασφάλιση ψηφιακά και φέρνουμε στην οθόνη σας τις καλύτερες επιλογές ταξιδιωτικής ασφάλισης.</h2>
 
             {/* Circles */}
-            <section className="flex flex-wrap gap-5 items-start justify-evenly mt-10 md:mt-[91px]">
+            <section className="flex flex-wrap gap-2 vsm:gap-5 items-start justify-evenly mt-10 md:mt-[91px]">
                 <CircleGray>Εύκολη Διαδικασία Αγοράς</CircleGray>
                 <CircleGray>Διαφορετικές Επιλογές</CircleGray>
                 <CircleGray>Άμεση Έκδοση Συμβολαίου</CircleGray>
@@ -18,7 +18,7 @@ export const WhyDigitalSection = () => {
 
             {/* Quote Button */}
             <button
-                className="w-28 h-12 sm:w-[213px] sm:h-[65px] rounded-[10px] bg-primaryBgColor text-primaryColor sm:text-lg font-bold mt-8 sm:mt-16 hover:bg-primaryColor hover:text-secondaryColor transition_all"
+                className="w-28 h-12 sm:w-[213px] sm:h-[65px] rounded-[10px] bg-primaryBgColor text-primaryColor text-xs tiny:text-base sm:text-lg font-bold mt-8 sm:mt-16 hover:bg-primaryColor hover:text-secondaryColor transition_all"
                 style={{ boxShadow: '0px 4px 4px 0px #00000026' }}
             >
                 Get Quote

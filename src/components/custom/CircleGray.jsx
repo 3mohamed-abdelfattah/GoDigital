14ec3ff30e63152c751074932c820fa994d26f5c
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-04 11:12:14 +0200

    Responsive Why Digital Section

--- src/components/custom/CircleGray.jsx
+++ src/components/custom/CircleGray.jsx
@@ -1,10 +1,10 @@
 export const CircleGray = ({ children }) => {
     return (
-        <span className="flex flex-col items-center gap-9">
+        <span className="flex flex-col items-center gap-3 sm:gap-9">
             {/* Circle */}
-            <span className="w-[100px] h-[100px] rounded-full bg-primaryBgColor"></span>
+            <span className="w-20 h-20 sm:w-[100px] sm:h-[100px] rounded-full bg-primaryBgColor"></span>
             {/* title */}
-            <h3 className="text-xl leading-[24.2px] text-center font-bold text-primaryColor max-w-[221px]">{children}</h3>
+            <h3 className="text-sm sm:text-xl sm:leading-[24.2px] text-center font-bold text-primaryColor max-w-[130px] sm:max-w-[221px]">{children}</h3>
         </span>
     )
 }

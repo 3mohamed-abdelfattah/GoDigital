d09657e4ba4b0032710707ddc017514ae17ff47e
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-09 21:40:38 +0200

    All Services Section in Products Pages

--- src/components/custom/CircleGray.jsx
+++ src/components/custom/CircleGray.jsx
@@ -1,10 +1,10 @@
-export const CircleGray = ({ children }) => {
+export const CircleGray = ({ children, circleColor = "primaryBgColor", textColor = "primaryColor" }) => {
     return (
         <span className="flex flex-col items-center tiny:gap-3 sm:gap-9">
             {/* Circle */}
-            <span className="w-12 h-12 tiny:w-20 tiny:h-20 sm:w-[100px] sm:h-[100px] rounded-full bg-primaryBgColor"></span>
+            <span className={`m-1 w-12 h-12 tiny:w-20 tiny:h-20 sm:w-[100px] sm:h-[100px] rounded-full bg-${circleColor}`}></span>
             {/* title */}
-            <h3 className="text-xs tiny:text-xs sm:text-xl sm:leading-[24.2px] text-center font-bold text-primaryColor max-w-[130px] sm:max-w-[221px]">{children}</h3>
+            <h3 className={`text-xs tiny:text-xs sm:text-xl sm:leading-[24.2px] text-center font-bold text-${textColor} w-[221px]`}>{children}</h3>
         </span>
     )
 }
--- src/pages/products/IntermediariesPage.jsx
+++ src/pages/products/IntermediariesPage.jsx
@@ -1,5 +1,5 @@
 // Components
-import { Header, HeroProductsSection, CircleDashed } from "@/components"
+import { Header, HeroProductsSection, CircleDashed, CircleGray } from "@/components"
 // Icons
 import * as Icons from "@/utils/icons.util"
 
@@ -14,7 +14,7 @@ export const IntermediariesPage = () => {
                 <input placeholder="Ταυτότητα" className="w-full max-w-[450px] xl:max-w-[680px] appearance-none h-14 md:h-[74px] bg-white border border-gray-300 text-sm vsm:text-base font-bold rounded-[10px] text-[#7D7D7D] py-2 px-4 pr-10 shadow focus:outline-none focus:ring-2 focus:ring-orange-400" />
             </HeroProductsSection>
 
-            {/* Circles Section */}
+            {/* How Work Section */}
             <section className="mb-0 sm:mb-24">
                 <h1 className="text-center text-2xl lg:text-[40px] font-bold my-10 sm:my-12">Πώς δουλεύει</h1>
                 <article className="flex flex-col sm:flex-row items-start vsm:items-stretch max-w-[1202px] mx-20 vsm:mx-10 xl:m-auto">
@@ -40,6 +40,37 @@ export const IntermediariesPage = () => {
                         <Icons.CashIcon />
                     </CircleDashed>
                 </article>
+            </section>
+
+            {/* Services Section */}
+            <section className="max-w-[1754px] bg-secondaryBgColor rounded-3xl lg:rounded-[58px] text-center mt-10 md:mt-[139px] mx-7 lg:mx-20 p-5 vsm:p-8 lg:p-12">
+                {/* Head Titles */}
+                <h1 className="font-extrabold text-3xl sm:text-[40px]">Υπηρεσίες</h1>
+                <h2 className="max-w-[911px] mt-3 font-medium text-xs tiny:text-lg sm:text-[22px] sm:leading-[30.05px] mx-auto">Στην GoDigital φροντίζουμε να σας προσφέρουμε κορυφαίες υπηρεσίες που να κάνουν την εμπειρία σας μοναδική.</h2>
+
+                {/* Circles */}
+                <article className="flex flex-wrap gap-2 vsm:gap-5 items-start justify-evenly mt-10 md:mt-[91px]">
+                    <CircleGray circleColor="secondaryColor" textColor="primaryBgColor">
+                        Γρήγορη Προσφορά
+                        <p className="font-normal mt-2 vsm:mt-5 text-center"> Πάρε σε λιγότερο από 1 λεπτό προσφορά για την ασφάλιση του ταξιδιού σου</p>
+                    </CircleGray>
+                    <CircleGray circleColor="secondaryColor" textColor="primaryBgColor">
+                        Ανθρώπινη Εξυπηρέτηση
+                        <p className="font-normal mt-5 text-center">Έχεις απορίες; Άσε μας τα στοιχεία σου και θα επικοινωνήσουμε άμεσα μαζί σου.</p>
+                    </CircleGray>
+                    <CircleGray circleColor="secondaryColor" textColor="primaryBgColor">
+                        Άμεση Διεκπεραίωση απαιτήσεων
+                        <p className="font-normal mt-5 text-center"> Φροντίζουμε να αποζημιώνεστε δίκαια και άμεσα</p>
+                    </CircleGray>
+                </article>
+
+                {/* Quote Button */}
+                <button
+                    className="w-28 h-12 sm:w-[213px] sm:h-[65px] rounded-[10px] bg-primaryBgColor text-primaryColor text-xs tiny:text-base sm:text-lg font-bold mt-8 sm:mt-16 hover:bg-secondaryColor hover:text-primaryColor transition_all active:scale-110"
+                    style={{ boxShadow: "0px 4px 4px 0px #00000026" }}
+                >
+                    Get Quote
+                </button>
             </section>
         </main>
     )

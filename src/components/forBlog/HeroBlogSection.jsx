31ad60366276d093a63e99f531a259e07278fdab
MohamedMohamed <mohamed.m.abdalfattah@gmail.com>
2024-12-09 18:01:47 +0200

    Cancel Clickable button functionality

--- src/components/forBlog/HeroBlogSection.jsx
+++ src/components/forBlog/HeroBlogSection.jsx
@@ -5,13 +5,6 @@ import Symbol from "@/assets/images/icon.png"
 import * as Icons from "@/utils/icons.util"
 
 export const HeroBlogSection = () => {
-    const [toggle, setToggle] = useState(false) // Slide Bar Toggle
-
-    // Function to invert state on click
-    const handleToggle = () => {
-        setToggle((prev) => !prev);
-    };
-
     return (
         <main className="bg-secondaryColor mx-6 lg:mx-20 rounded-3xl md:rounded-[58px]">
 
@@ -27,15 +20,9 @@ export const HeroBlogSection = () => {
                 <h2 className="mt-5 mb-6 mx-5 max-w-[519px] text-xs vsm:text-base md:text-[22px] vsm:leading-[30.05px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h2>
             </section>
 
-            {/* Toggle Bar [Travel Insurance] */}
-            <section className={`flex flex-wrap justify-center items-center gap-10 rounded-3xl md:rounded-[58px] text-center text-primaryColor py-4 md:py-8 mx-5 md:mx-20 transition_all ${toggle ? 'max-h-96' : 'max-h-32'}`}>
-                {toggle ?
-                    <span className="flex flex-col justify-center items-center gap-4 md:gap-7">
-                        <Icons.PlaneIcon />
-                        <h3 className="font-bold md:text-3xl text-center">Travel Insurance</h3>
-                        <span onClick={handleToggle} className="text-primaryColor hover:text-primaryBgColor rotate-180 cursor-pointer"><Icons.BottomArrowIcon /></span>
-                    </span>
-                    : <span onClick={handleToggle} className="text-primaryBgColor hover:text-primaryColor cursor-pointer"><Icons.BottomArrowIcon /></span>}
+            {/* Arrow */}
+            <section className={`flex justify-center items-center gap-10 rounded-3xl md:rounded-[58px] py-4 md:py-8 mx-5 md:mx-20`}>
+                <span className="text-primaryBgColor"><Icons.BottomArrowIcon /></span>
             </section>
 
         </main>
